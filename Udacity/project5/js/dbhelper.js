8047feea32e86ee7c941b39a966741d2664d76bb
zainabhath <zainb-hathloul@hotmail.com>
2019-06-12 10:47:11 +0300

    add dot to each url to solve load issue

--- Udacity/project5/js/dbhelper.js
+++ Udacity/project5/js/dbhelper.js
@@ -152,7 +152,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
