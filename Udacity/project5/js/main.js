25545680d6be336139ba94917ebcbd6f32bd586e
zainabhath <zainb-hathloul@hotmail.com>
2019-06-12 11:07:07 +0300

    add magic dot to service worker url

--- Udacity/project5/js/main.js
+++ Udacity/project5/js/main.js
@@ -214,7 +214,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 //service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+    navigator.serviceWorker.register('./serviceWorker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
