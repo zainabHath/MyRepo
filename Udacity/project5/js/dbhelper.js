4aaa99ca798b5468fd17a4d7223e90fb4f7cc41a
zainabhath <zainb-hathloul@hotmail.com>
2019-06-12 10:40:34 +0300

    update url

--- Udacity/project5/js/dbhelper.js
+++ Udacity/project5/js/dbhelper.js
@@ -8,9 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8088 // Change this to your server port
+    const port = 8000 // Change this to your server port
     //return `http://localhost:${port}/data/restaurants.json`;
-    return `https://zainabhath.github.io/MyRepo/Udacity/project5/data/restaurants.json`;
+    return `./data/restaurants.json`;
   }
 
   /**
