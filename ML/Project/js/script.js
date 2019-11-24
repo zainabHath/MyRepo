6a8d7319f8f989e01eca8b4535f4d4add8e09898
zainabHath <zainab-hathloul@hotmail.com>
2019-11-24 13:04:59 +0300

    Update script.js

--- ML/Project/js/script.js
+++ ML/Project/js/script.js
@@ -17,48 +17,15 @@ function optionPresed(){
     liveSelected=true;
   }
 }
-/*  const data = await d3.csv('datat.csv');
-  // do whatever with data here
-  console.log(data[1]);
 
-}
-
-doThings();*/
 let Data=null;
 d3.csv('datat.csv', function(error, data) {
   if (error) throw error;
   Data=Array.from(data);
-  console.log(typeof data);
-  console.log(data[0]);
-  console.log(data[1]);
-
-});
-console.log(Data[1]);
-let dat=[{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jan", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jun", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "pos"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Mar", Label: "Neg"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "pos"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Mar", Label: "Neg"},
-{CreatedAt: "Mar", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jun", Label: "pos"}, {CreatedAt: "Jun", Label: "Neg"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jun", Label: "pos"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jun", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Aug", Label: "pos"}, {CreatedAt: "Aug", Label: "pos"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Aug", Label: "Neg"},
-{CreatedAt: "Aug", Label: "Neg"}, {CreatedAt: "Aug", Label: "pos"},
-{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Jun", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
-{CreatedAt: "Nov", Label: "pos"}, {CreatedAt: "Nov", Label: "pos"},
-{CreatedAt: "Nov", Label: "Neg"}, {CreatedAt: "Nov", Label: "Neg"},
-{CreatedAt: "Nov", Label: "Neg"}, {CreatedAt: "Nov", Label: "pos"}]
-var entries = d3.nest()
+ var entries = d3.nest()
     .key(function(d) { return d.CreatedAt; })
     .key(function(d) { return d.Label; })
-    .entries(Data);
+    .entries(data);
 console.log(entries);
 var Monthes = entries.map(a => a.key);
 var values = entries.map(a => a.values);
@@ -90,44 +57,6 @@ for(var item of values){
 console.log(valsN);
 console.log(valsP);
 var ctx = document.getElementById("chart-0").getContext("2d");
-/*
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [0, 1, 0, 1, 1, 0],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-*/
 new Chart(ctx, {
 type: 'line',
 data: {
@@ -179,3 +108,28 @@ yAxes: [{
 }
 }
 });
+
+});
+console.log(Data[1]);
+let dat=[{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jan", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jun", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "pos"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Mar", Label: "Neg"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "pos"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Mar", Label: "Neg"},
+{CreatedAt: "Mar", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jun", Label: "pos"}, {CreatedAt: "Jun", Label: "Neg"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jun", Label: "pos"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jun", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Aug", Label: "pos"}, {CreatedAt: "Aug", Label: "pos"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Aug", Label: "Neg"},
+{CreatedAt: "Aug", Label: "Neg"}, {CreatedAt: "Aug", Label: "pos"},
+{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Jun", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
+{CreatedAt: "Nov", Label: "pos"}, {CreatedAt: "Nov", Label: "pos"},
+{CreatedAt: "Nov", Label: "Neg"}, {CreatedAt: "Nov", Label: "Neg"},
+{CreatedAt: "Nov", Label: "Neg"}, {CreatedAt: "Nov", Label: "pos"}]
+
