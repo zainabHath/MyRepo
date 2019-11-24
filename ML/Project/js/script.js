3fa253cc09402ce96e722913307a85ea90fe1a95
zainabHath <zainab-hathloul@hotmail.com>
2019-11-24 13:00:04 +0300

    Update script.js

--- ML/Project/js/script.js
+++ ML/Project/js/script.js
@@ -33,7 +33,7 @@ d3.csv('datat.csv', function(error, data) {
   console.log(data[1]);
 
 });
-//console.log(Data[1]);
+console.log(Data[1]);
 let dat=[{CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
 {CreatedAt: "Jan", Label: "Neg"}, {CreatedAt: "Jan", Label: "Neg"},
 {CreatedAt: "Jan", Label: "pos"}, {CreatedAt: "Jan", Label: "Neg"},
